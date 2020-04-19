9734e03853c1d70f2b1c10eff70d81a796f5ba0d
Derrick Beining <derrick.beining@gmail.com>
2020-04-18 23:34:47 -0400

    chore(tools): use es6 import in tools/semantic-release-prepare.ts

--- tools/semantic-release-prepare.ts
+++ tools/semantic-release-prepare.ts
@@ -1,14 +1,20 @@
-const path = require('path')
-const { fork } = require('child_process')
-const colors = require('colors')
+import { fork } from 'child_process'
+import colors from 'colors'
+import { writeFileSync } from 'fs'
+import path from 'path'
 
-const { readFileSync, writeFileSync } = require('fs')
-const pkg = JSON.parse(readFileSync(path.resolve(__dirname, '..', 'package.json')))
+import pkg from '../package.json'
 
-pkg.scripts.prepush = 'npm run test:prod && npm run build'
-pkg.scripts.commitmsg = 'commitlint -E HUSKY_GIT_PARAMS'
+const scripts = {
+  ...pkg.scripts,
+  prepush: 'npm run test:prod && npm run build',
+  commitmsg: 'commitlint -E HUSKY_GIT_PARAMS',
+}
 
-writeFileSync(path.resolve(__dirname, '..', 'package.json'), JSON.stringify(pkg, null, 2))
+writeFileSync(
+  path.resolve(__dirname, '..', 'package.json'),
+  JSON.stringify({ ...pkg, scripts }, null, 2)
+)
 
 // Call husky to set up the hooks
 fork(path.resolve(__dirname, '..', 'node_modules', 'husky', 'lib', 'installer', 'bin'), ['install'])
