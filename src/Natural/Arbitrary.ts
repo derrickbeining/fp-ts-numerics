aa86db46c087be9c5089fb99a8ced091bb0405d8
Derrick Beining <derrick.beining@gmail.com>
2020-04-21 02:51:00 -0400

    feat: add Natural/Arbitrary.ts

---
+++ src/Natural/Arbitrary.ts
@@ -0,0 +1,13 @@
+import * as fc from 'fast-check'
+
+import { Natural } from '../Natural'
+
+/**
+ *  `fast-check` Arbitrary instance
+ *
+ * @since 1.0.0
+ */
+export const arbitraryNatural: fc.Arbitrary<Natural> = fc
+  .maxSafeInteger()
+  .filter((n) => n >= 0)
+  .map((n) => Natural.unsafeFromNumber(n))
