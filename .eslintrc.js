b619684dd5b163d9f3c170215810407302e5803b
Derrick Beining <derrick.beining@gmail.com>
2020-04-21 02:05:32 -0400

    chore: install linting deps directly and use eslint instead of tsdx lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,24 @@
 module.exports = {
-  extends: ['react-app', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
-  plugins: ['simple-import-sort'],
-
+  extends: [
+    // 'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    // 'plugin:@typescript-eslint/recommended',
+    // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+  plugins: ['simple-import-sort', 'prettier', '@typescript-eslint'],
+  root: true,
   rules: {
     'simple-import-sort/sort': 'error',
-  },
+    'prettier/prettier': 'error',
 
-  settings: {
-    react: {
-      version: '999.999.999',
-    },
+    // '@typescript-eslint/explicit-function-return-type': 0,
+    // '@typescript-eslint/no-empty-interface': 0,
   },
 }
